3c9db79756f653b2a61905db9255aa09b5242ad4
Bharat Sonwane <bharatrsonwane@gmail.com>
2022-09-30 23:23:05 +0530

    app.module,controller added in app folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
